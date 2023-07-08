795f9097c142f693de1e6356263f924cf7302c45
Your Name <sm920149@gmail.com>
2023-07-08 22:51:56 +0530

    backend

--- Zomato.py
+++ Zomato.py
@@ -1,29 +1,43 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import json
 
 app = Flask(__name__)
 
-# Dummy data
-dishes =  [
-    {"id": 1, "name": "Pizza", "price": 299.99, "availability": True},
-    {"id": 2, "name": "Burger", "price": 249.99, "availability": True},
-    {"id": 3, "name": "Pasta", "price": 219.99, "availability": False},
-    {"id": 4, "name": "Salad", "price": 249.99, "availability": True},
-    {"id": 5, "name": "Sushi", "price": 329.99, "availability": True},
-    {"id": 6, "name": "Steak", "price": 349.99, "availability": False},
-    {"id": 7, "name": "Sandwich", "price": 229.99, "availability": True},
-    {"id": 8, "name": "Soup", "price": 209.99, "availability": True},
-    {"id": 9, "name": "Chicken Wings", "price": 289.99, "availability": True},
-    {"id": 10, "name": "Fish and Chips", "price": 249.99, "availability": False},
-    {"id": 11, "name": "Ice Cream", "price": 209.99, "availability": True},
-    {"id": 12, "name": "Pancakes", "price": 239.99, "availability": True}
-]
+CORS(app)
 
-orders = []
+# MongoDB connection
+client = MongoClient("mongodb+srv://shoaib:shoaibmansuri@cluster0.zghdsdl.mongodb.net/?retryWrites=true&w=majority")
+db = client["<Zomato_db>"]
+dishes_collection = db["dishes"]
+orders_collection = db["orders"]
+
+
+
+@app.route('/bah')
+def index():
+    menu_data = list(orders_collection.find())
+    menu_data = json.loads(json.dumps(orders_collection, default=str))
+    return menu_data
+
+
+
+@app.route('/review_orders')
+def review_orders():
+    orders_data = list(orders_collection.find())
+    orders_data = json.loads(json.dumps(orders_data, default=str))
+    return orders_data
+
 
 
 @app.route('/')
-def index():
-    return jsonify(dishes=dishes, orders=orders)
+def review_order():
+    orders_data = list(dishes_collection.find())
+    orders_data = json.loads(json.dumps(dishes_collection, default=str))
+    return orders_data
+
 
 
 @app.route('/add_dish', methods=['POST'])
@@ -33,23 +47,31 @@ def add_dish():
     dish_price = float(data['price'])
     dish_availability = True if data.get('availability') else False
 
-    dish_id = len(dishes) + 1
-    dish = {"id": dish_id, "name": dish_name, "price": dish_price, "availability": dish_availability}
-    dishes.append(dish)
+    dish = {
+        "name": dish_name,
+        "price": dish_price,
+        "availability": dish_availability
+    }
+
+    dishes_collection.insert_one(dish)
 
     return jsonify(success=True)
 
 
-@app.route('/dishes/<int:dish_id>', methods=['PATCH'])
+@app.route('/dishes/<string:dish_id>', methods=['PATCH'])
 def update_availability(dish_id):
-    availability = request.get_json().get('availability')
+    data = request.get_json()
+    availability = data.get('availability')
 
-    for dish in dishes:
-        if dish['id'] == dish_id:
-            dish['availability'] = True if availability else False
-            break
+    result = dishes_collection.update_one(
+        {"_id": ObjectId(dish_id)},
+        {"$set": {"availability": availability}}
+    )
 
-    return jsonify(success=True)
+    if result.modified_count == 1:
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False, message="Failed to update the dish availability.")
 
 
 @app.route('/take_order', methods=['POST'])
@@ -59,13 +81,17 @@ def take_order():
     dish_ids = [data['dish_ids']] if isinstance(data['dish_ids'], int) else data['dish_ids']
 
     for dish_id in dish_ids:
-        dish = next((item for item in dishes if item['id'] == dish_id), None)
+        dish = dishes_collection.find_one({"_id": ObjectId(dish_id)})
         if dish is None or not dish['availability']:
             return jsonify(success=False, message="Sorry, the requested dish is not available.")
 
-    order_id = len(orders) + 1
-    order = {"id": data['id'], "customer_name": customer_name, "dish_ids": dish_ids, "status": data['status']}
-    orders.append(order)
+    order = {
+        "customer_name": customer_name,
+        "dish_ids": [ObjectId(dish_id) for dish_id in dish_ids],
+        "status": data['status']
+    }
+
+    orders_collection.insert_one(order)
 
     return jsonify(success=True)
 
@@ -73,29 +99,32 @@ def take_order():
 @app.route('/update_order_status', methods=['POST'])
 def update_order_status():
     data = request.get_json()
-    order_id = int(data['order_id'])
+    order_id = data['id']
     status = data['status']
 
-    for order in orders:
-        if order['id'] == order_id:
-            order['status'] = status
-            break
+    result = orders_collection.update_one(
+        {"_id": ObjectId(order_id)},
+        {"$set": {"status": status}}
+    )
 
-    return jsonify(success=True)
-
-
-@app.route('/review_orders')
-def review_orders():
-    return jsonify(orders=orders)
+    if result.modified_count == 1:
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False, message="Failed to update the order status.")
 
 
-@app.route('/remove_dish/<int:dish_id>', methods=['DELETE'])
+
+
+
+@app.route('/remove_dish/<string:dish_id>', methods=['DELETE'])
 def remove_dish(dish_id):
-    global dishes
+    result = dishes_collection.delete_one({"_id": ObjectId(dish_id)})
 
-    dishes = [dish for dish in dishes if dish['id'] != dish_id]
+    if result.deleted_count == 1:
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False, message="Failed to remove the dish.")
 
-    return jsonify(success=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
